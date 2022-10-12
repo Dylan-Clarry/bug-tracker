e630b414778a1b388adbfd65851f25f48f7fdc49
Dylan <dylan.imail@gmail.com>
2022-10-12 17:45:03 -0400

    tailwind setup

--- src/Components/BugEntry.tsx
+++ src/Components/BugEntry.tsx
@@ -1,9 +1,19 @@
 import React from "react";
 
-const BugEntry: React.FC = () => {
+interface IBugEntryProps {
+  bug: Bug;
+}
+
+const BugEntry: React.FC<IBugEntryProps> = ({ bug }) => {
   return (
     <div className="BugEntry">
       <h2>Bug Entry</h2>
+      <ul className="underline">
+        <li>{bug.id}</li>
+        <li>{bug.title}</li>
+        <li>{bug.text}</li>
+        <li>{bug.closed}</li>
+      </ul>
     </div>
   );
 };
--- src/Components/BugList.tsx
+++ src/Components/BugList.tsx
@@ -1,13 +1,24 @@
 import React from "react";
 import BugEntry from "./BugEntry";
 
+const testBugList: Bug[] = [
+  {
+    id: 1,
+    title: "Test Bug",
+    text: "This is a test and you failed the test!!!",
+    closed: false,
+  },
+];
+
 const BugList: React.FC = () => {
   return (
     <div className="BugList">
       <h1>BugList</h1>
-      <BugEntry />
-      <BugEntry />
-      <BugEntry />
+      <ul>
+        {testBugList.map((bug) => {
+          return <BugEntry bug={bug} />;
+        })}
+      </ul>
     </div>
   );
 };
